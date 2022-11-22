11cf16b1ee6bcbee1bdc463bfb6eb641c43ad9b6
Bibek Bhattarai <bibekjodd@outlook.com>
2022-11-22 21:15:33 +0545

    let-const

---
+++ letconst.js
@@ -0,0 +1,22 @@
+// priority 
+// const 
+// let 
+// var 
+
+
+
+// -------- type error --------
+// const a = 3;
+// a = 5;
+
+
+// -------- syntax error --------
+// const a;
+// let b = 3;
+// let b = 7;
+
+
+
+// -------- Reference Error --------
+// console.log(x);
+// let x = 7;   x is in temporal dead zone
