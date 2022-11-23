798295433815ec0cd2cee32cabf94358c5fa2a37
Bibek Bhattarai <bibekjodd@outlook.com>
2022-11-23 13:42:52 +0545

    data-hiding constuctor function

--- Closures.js
+++ Closures.js
@@ -13,17 +13,17 @@
 
 
 
-function x() {
-    // function bundled together along with its lexical scope is closure
-    var a = 2;
-    function y() {
-        console.log(a)
-    }
-    return y; // it not only returns y but also the reference to var a.
-}
+// function x() {
+//     // function bundled together along with its lexical scope is closure
+//     var a = 2;
+//     function y() {
+//         console.log(a)
+//     }
+//     return y; // it not only returns y but also the reference to var a.
+// }
 
-var a = 7;
-x()();  // 2
+// var a = 7;
+// x()();  // 2
 
 // -------- closures uses --------
 // module design pattern
@@ -33,4 +33,63 @@ x()();  // 2
 // maintaining state in async world
 // settimeouts
 // iterators
-// ... 
+// and ...
+
+
+
+
+
+// function x() {
+//     for (var i = 1; i <= 5; i++) {
+//         function close(i) {
+//             setTimeout(() => {
+//                 console.log(i)
+//                 console.log(i *= 2)
+//             }, 1000 * i);
+//         }
+//         close(i);
+//     }
+//     console.log('hello');
+// }
+
+// x();
+
+
+// -------- data hiding --------
+// function counter() {
+//     function incrementCounter() {
+//         count++;
+//         console.log(count);
+//     }
+//     let count = 1;
+//     return incrementCounter
+// }
+
+// const counter1 = counter();
+// counter1() // 2
+// counter1() // 3
+// counter1() // 4
+
+// const counter2 = counter();
+// counter2(); // 2
+
+// const counter3 = counter1;
+// counter3(); // 5 
+
+function ConstructorFunction() {
+    this.count = 9;
+    count = 0;
+    this.incrementCounter = function () {
+        count++;
+        console.log(count);
+    }
+    this.decrementCounter = () => {
+        count--;
+        console.log(count);
+    }
+}
+
+const newObj = new ConstructorFunction();
+newObj.incrementCounter();
+newObj.decrementCounter();
+console.log(newObj.count)
