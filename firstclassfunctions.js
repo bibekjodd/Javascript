9ceeede15ca812ef9f805ab1ebe6bae0366d3b3f
Bibek Bhattarai <bibekjodd@outlook.com>
2022-11-23 14:03:36 +0545

    first-class-functions

---
+++ firstclassfunctions.js
@@ -0,0 +1,24 @@
+
+
+
+// -------- Function Statement --------
+// function a() {
+// console.log('a')
+// }
+
+
+// -------- Function Expression --------
+// const b = function () {
+// console.log('b')
+// }
+
+
+
+// -------- Anonymous Function --------
+// function() {
+// must be assigend to  variable
+// }
+
+
+// -------- First Class Function --------
+// ability of functions to be used as values
